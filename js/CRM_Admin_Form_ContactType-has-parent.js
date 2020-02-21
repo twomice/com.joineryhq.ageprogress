7b6219cce50391fe9f763c62370d88a964a7f5c9
Allen Shaw <allen@JoineryHQ.com>
2020-02-20 18:33:24 -0600

    Added on-page show-hide js for Contact Type edit.

--- js/CRM_Admin_Form_ContactType-has-parent.js
+++ js/CRM_Admin_Form_ContactType-has-parent.js
@@ -1,5 +1,28 @@
 (function(ts) {
   CRM.$(function($) {
+
+    var isAgeprogressChange = function isAgeprogressChange() {
+      var finalTr = $('#is_ageprogress_final').closest('tr');
+      var maxAgeTr = $('#ageprogress_max_age').closest('tr');
+      if ($('#is_ageprogress').is(':checked')) {
+        finalTr.show();
+        isAgeprogressFinalChange();
+      }
+      else {
+        finalTr.hide();
+        maxAgeTr.hide();
+      }
+    }
+
+    var isAgeprogressFinalChange = function isAgeprogressFinalChange() {
+      var tr = $('#ageprogress_max_age').closest('tr');
+      if ($('#is_ageprogress_final').is(':checked')) {
+        tr.hide();
+      }
+      else {
+        tr.show();
+      }
+    }
     // Give the bhfe elements table an id so we can handle it later.
     $('input#is_ageprogress').closest('table').attr('id', 'bhfe_table');
 
@@ -9,15 +32,15 @@
     // Move all bhfe table rows into the main table aftrer 'is active'
     $('table#bhfe_table tr').insertAfter(trIsActive);
 
-//    // Set change hanler for 'is_multiple', and go ahead and run it to start with.
-//    $('input#is_multiple_registrations').change(isMultipleRegistrationsChange);
-//    isMultipleRegistrationsChange();
-//
-//    // Set change hanler for 'is_primary_atending' radios.
-//    $('input[name="is_primary_attending"]').change(isPrimaryAttendingChange);
-
     // Remove the bhfe table, which should be empty by now.
     $('table#bhfe_table').remove();
+
+    // Set change hanler for 'is_ageprogress_final'.
+    $('input#is_ageprogress_final').change(isAgeprogressFinalChange);
+
+    // Set change hanler for 'is_ageprogress', and go ahead and run it to start with.
+    $('input#is_ageprogress').change(isAgeprogressChange);
+    isAgeprogressChange();
 
   });
 }(CRM.ts('com.joineryhq.ageprogress')));
