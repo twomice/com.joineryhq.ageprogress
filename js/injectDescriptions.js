2fa07207ba0e37e90e40ba4e147996bb43d46602
Allen Shaw <allen@JoineryHQ.com>
2020-02-20 18:37:57 -0600

    bug-fix/typo: discriptions were not showing up.

--- js/injectDescriptions.js
+++ js/injectDescriptions.js
@@ -2,7 +2,7 @@
   CRM.$(function($) {
     // Append any descriptions for bhfe fields.
     for (var i in CRM.vars.ageprogress.descriptions) {
-      $('input#' + i + ', select#' + i).after('<div class="description" id="' + i + '-description>'+ CRM.vars.ageprogress.descriptions[i] +'</div>');
+      $('input#' + i + ', select#' + i).after('<div class="description" id="' + i + '-description">'+ CRM.vars.ageprogress.descriptions[i] +'</div>');
     }
 
   });
